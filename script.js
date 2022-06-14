8f705fc03a18a22ec5299cd1b2e2d1a719094a61
audriusxp <palm@inbox.lt>
2022-06-14 11:22:08 +0300

    patikrinta ar visi skaiciai dalinasi is 3 5 ir 10

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 window.onload = function () {
-    let x = 10;
-    let y = 25;
-    let z =27;
+    let x = 90;
+    let y = 10;
+    let z =-100;
+    console.log(arSkaiciauTrysPenkiDesimtKartotinis(90))
+    console.log(arSkaiciauTrysPenkiDesimtKartotinis(10))
+    console.log(arSkaiciauTrysPenkiDesimtKartotinis(-100))
     
+
+}
+function arSkaiciauTrysPenkiDesimtKartotinis(skaicius)
+
+{ if ((skaicius %3===0)&& (skaicius % 5 === 0)&&(skaicius %10 === 0))
+    { return true ;
+    }
+  else ( (skaicius %3 >0)&&(skaicius %5 > 0)&&(skaicius % 10 >0)) 
+  {
+    return false; 
+ }
+
 }
